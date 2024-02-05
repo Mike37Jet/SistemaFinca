040687b6daf65a709d73e03cea0c944059c902ea
Mike37Jet <migue030603@gmail.com>
2024-02-04 23:31:28 -0500

    Agregar archivos de proyecto.

---
+++ SistemaFinca/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace SistemaFinca
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            panel1 = new Panel();
+            pictureBox3 = new PictureBox();
+            textUsuario = new TextBox();
+            textContraseña = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            linkLabel1 = new LinkLabel();
+            button2 = new Button();
+            button3 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 122, 204);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(250, 330);
+            panel1.TabIndex = 0;
+            panel1.Paint += panel1_Paint;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(3, 0);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(247, 327);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 0;
+            pictureBox3.TabStop = false;
+            // 
+            // textUsuario
+            // 
+            textUsuario.BackColor = Color.FromArgb(15, 15, 15);
+            textUsuario.BorderStyle = BorderStyle.None;
+            textUsuario.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textUsuario.ForeColor = Color.Gray;
+            textUsuario.Location = new Point(313, 74);
+            textUsuario.Name = "textUsuario";
+            textUsuario.Size = new Size(402, 25);
+            textUsuario.TabIndex = 1;
+            textUsuario.Text = "USUARIO";
+            textUsuario.TextChanged += textBox1_TextChanged;
+            textUsuario.Enter += textUsuario_Enter;
+            textUsuario.Leave += textUsuario_Leave;
+            // 
+            // textContraseña
+            // 
+            textContraseña.BackColor = Color.FromArgb(15, 15, 15);
+            textContraseña.BorderStyle = BorderStyle.None;
+            textContraseña.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textContraseña.ForeColor = Color.Gray;
+            textContraseña.Location = new Point(313, 140);
+            textContraseña.Name = "textContraseña";
+            textContraseña.Size = new Size(402, 25);
+            textContraseña.TabIndex = 2;
+            textContraseña.Text = "CONTRASEÑA";
+            textContraseña.Enter += textContraseña_Enter;
+            textContraseña.Leave += textContraseña_Leave;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.DimGray;
+            label1.Location = new Point(461, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(126, 40);
+            label1.TabIndex = 3;
+            label1.Text = "LOGIN";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(40, 40, 40);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(28, 28, 28);
+            button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(64, 64, 64);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.LightGray;
+            button1.Location = new Point(313, 216);
+            button1.Name = "button1";
+            button1.Size = new Size(402, 40);
+            button1.TabIndex = 3;
+            button1.Text = "ACCEDER";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.ActiveLinkColor = Color.FromArgb(0, 122, 204);
+            linkLabel1.AutoSize = true;
+            linkLabel1.LinkColor = Color.DimGray;
+            linkLabel1.Location = new Point(421, 277);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(197, 20);
+            linkLabel1.TabIndex = 0;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "¿Ha olvidado la contraseña?";
+            // 
+            // button2
+            // 
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            button2.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.Location = new Point(737, 9);
+            button2.Name = "button2";
+            button2.Size = new Size(31, 30);
+            button2.TabIndex = 6;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            button3.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.Location = new Point(696, 9);
+            button3.Name = "button3";
+            button3.Size = new Size(30, 30);
+            button3.TabIndex = 7;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // FormLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(15, 15, 15);
+            ClientSize = new Size(780, 330);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(linkLabel1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textContraseña);
+            Controls.Add(textUsuario);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "FormLogin";
+            Opacity = 0.9D;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "A-Boost";
+            Paint += FormLogin_Paint;
+            MouseDown += FormLogin_MouseDown;
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TextBox textUsuario;
+        private TextBox textContraseña;
+        private Label label1;
+        private Button button1;
+        private LinkLabel linkLabel1;
+        private PictureBox pictureBox3;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ SistemaFinca/Form1.cs
@@ -0,0 +1,97 @@
+using System.Runtime.InteropServices;
+namespace SistemaFinca
+{
+
+    public partial class FormLogin : Form
+    {
+
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendeMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+        private void FormLogin_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics login;
+            login = CreateGraphics();
+            Pen lapiz = new Pen(Color.Gray);
+            login.DrawLine(lapiz, 310, 100, 717, 100);
+            login.DrawLine(lapiz, 310, 165, 717, 165);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textUsuario_Enter(object sender, EventArgs e)
+        {
+            if (textUsuario.Text == "USUARIO")
+            {
+                textUsuario.Text = "";
+                textUsuario.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void textUsuario_Leave(object sender, EventArgs e)
+        {
+            if (textUsuario.Text == "")
+            {
+                textUsuario.Text = "USUARIO";
+                textUsuario.ForeColor = Color.DimGray;
+            }
+        }
+
+        private void textContraseña_Enter(object sender, EventArgs e)
+        {
+            if (textContraseña.Text == "CONTRASEÑA")
+            {
+                textContraseña.Text = "";
+                textContraseña.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void textContraseña_Leave(object sender, EventArgs e)
+        {
+            if (textContraseña.Text == "")
+            {
+                textContraseña.Text = "CONTRASEÑA";
+                textContraseña.ForeColor = Color.DimGray;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void FormLogin_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendeMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormMenu formMenu = new FormMenu();
+            formMenu.Show(this);
+        }
+        
+       
+    }
+}
