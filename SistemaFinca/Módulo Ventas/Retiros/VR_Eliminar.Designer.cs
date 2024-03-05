23a7cd620ddb64628eee7252d7c5fccf66fa2914
Mike37Jet <migue030603@gmail.com>
2024-03-04 23:36:38 -0500

    Cambios en el eliminar retiro, mejorado

--- SistemaFinca/Módulo Ventas/Retiros/VR_Eliminar.Designer.cs
+++ SistemaFinca/Módulo Ventas/Retiros/VR_Eliminar.Designer.cs
@@ -28,29 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
             btnEliminar = new Button();
             btnBuscar = new Button();
             label18 = new Label();
             label6 = new Label();
             txtCedula = new TextBox();
-            lstRetiros = new ListView();
-            columnCantidad = new ColumnHeader();
-            columnHeader1 = new ColumnHeader();
-            columnHeader2 = new ColumnHeader();
+            lstRetiros = new DataGridView();
+            ColumnCantRetiro = new DataGridViewTextBoxColumn();
+            ColumnFecha = new DataGridViewTextBoxColumn();
+            ColumnEstado = new DataGridViewTextBoxColumn();
+            ColumnID = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)lstRetiros).BeginInit();
             SuspendLayout();
             // 
             // btnEliminar
             // 
             btnEliminar.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             btnEliminar.Font = new Font("Segoe UI", 11.25F);
-            btnEliminar.Location = new Point(404, 444);
+            btnEliminar.Location = new Point(388, 476);
             btnEliminar.Margin = new Padding(3, 2, 3, 2);
             btnEliminar.Name = "btnEliminar";
-            btnEliminar.Size = new Size(82, 36);
+            btnEliminar.Size = new Size(98, 41);
             btnEliminar.TabIndex = 103;
             btnEliminar.Text = "Eliminar";
             btnEliminar.UseVisualStyleBackColor = true;
-            btnEliminar.Click += btnRegistrar_Click;
+            btnEliminar.Click += btnEliminar_Click;
             // 
             // btnBuscar
             // 
@@ -99,34 +104,76 @@
             // 
             // lstRetiros
             // 
+            lstRetiros.AllowUserToAddRows = false;
+            lstRetiros.AllowUserToDeleteRows = false;
+            lstRetiros.AllowUserToResizeColumns = false;
+            lstRetiros.AllowUserToResizeRows = false;
             lstRetiros.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            lstRetiros.Columns.AddRange(new ColumnHeader[] { columnCantidad, columnHeader1, columnHeader2 });
-            lstRetiros.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lstRetiros.HeaderStyle = ColumnHeaderStyle.Nonclickable;
-            lstRetiros.Location = new Point(212, 168);
-            lstRetiros.Margin = new Padding(3, 2, 3, 2);
-            lstRetiros.MultiSelect = false;
+            lstRetiros.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            lstRetiros.BackgroundColor = Color.FromArgb(45, 66, 91);
+            lstRetiros.BorderStyle = BorderStyle.None;
+            lstRetiros.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            lstRetiros.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = SystemColors.HotTrack;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle1.ForeColor = Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            lstRetiros.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            lstRetiros.ColumnHeadersHeight = 30;
+            lstRetiros.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            lstRetiros.Columns.AddRange(new DataGridViewColumn[] { ColumnCantRetiro, ColumnFecha, ColumnEstado, ColumnID });
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = SystemColors.Window;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            lstRetiros.DefaultCellStyle = dataGridViewCellStyle2;
+            lstRetiros.EnableHeadersVisualStyles = false;
+            lstRetiros.GridColor = Color.SteelBlue;
+            lstRetiros.Location = new Point(68, 174);
             lstRetiros.Name = "lstRetiros";
-            lstRetiros.Size = new Size(454, 261);
+            lstRetiros.ReadOnly = true;
+            lstRetiros.RowHeadersVisible = false;
+            dataGridViewCellStyle3.BackColor = Color.FromArgb(45, 66, 91);
+            dataGridViewCellStyle3.ForeColor = Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = Color.SteelBlue;
+            dataGridViewCellStyle3.SelectionForeColor = Color.White;
+            lstRetiros.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            lstRetiros.RowTemplate.Height = 25;
+            lstRetiros.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            lstRetiros.Size = new Size(762, 282);
             lstRetiros.TabIndex = 110;
-            lstRetiros.UseCompatibleStateImageBehavior = false;
-            lstRetiros.View = View.Details;
-            lstRetiros.ItemSelectionChanged += lstRetiros_ItemSelectionChanged;
+            lstRetiros.CellClick += lstRetiros_CellClick;
             // 
-            // columnCantidad
+            // ColumnCantRetiro
             // 
-            columnCantidad.Text = "Cantidad de Retiro";
-            columnCantidad.Width = 140;
+            ColumnCantRetiro.HeaderText = "Cantidad de retiro";
+            ColumnCantRetiro.Name = "ColumnCantRetiro";
+            ColumnCantRetiro.ReadOnly = true;
             // 
-            // columnHeader1
+            // ColumnFecha
             // 
-            columnHeader1.Text = "Fecha";
-            columnHeader1.Width = 140;
+            ColumnFecha.HeaderText = "Fecha";
+            ColumnFecha.Name = "ColumnFecha";
+            ColumnFecha.ReadOnly = true;
             // 
-            // columnHeader2
+            // ColumnEstado
             // 
-            columnHeader2.Text = "Estado";
-            columnHeader2.Width = 120;
+            ColumnEstado.HeaderText = "Estado";
+            ColumnEstado.Name = "ColumnEstado";
+            ColumnEstado.ReadOnly = true;
+            // 
+            // ColumnID
+            // 
+            ColumnID.HeaderText = "Id";
+            ColumnID.Name = "ColumnID";
+            ColumnID.ReadOnly = true;
+            ColumnID.Visible = false;
             // 
             // FormVR_Eliminar
             // 
@@ -143,6 +190,7 @@
             Margin = new Padding(3, 2, 3, 2);
             Name = "FormVR_Eliminar";
             Text = "Administrador";
+            ((System.ComponentModel.ISupportInitialize)lstRetiros).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -162,9 +210,10 @@
         private DataGridViewTextBoxColumn Column2;
         private DataGridViewTextBoxColumn Column4;
         private DataGridViewTextBoxColumn Column5;
-        private ListView lstRetiros;
-        private ColumnHeader columnCantidad;
-        private ColumnHeader columnHeader1;
-        private ColumnHeader columnHeader2;
+        private DataGridView lstRetiros;
+        private DataGridViewTextBoxColumn ColumnCantRetiro;
+        private DataGridViewTextBoxColumn ColumnFecha;
+        private DataGridViewTextBoxColumn ColumnEstado;
+        private DataGridViewTextBoxColumn ColumnID;
     }
 }
--- SistemaFinca/Módulo Ventas/Retiros/VR_Eliminar.cs
+++ SistemaFinca/Módulo Ventas/Retiros/VR_Eliminar.cs
@@ -22,50 +22,56 @@ namespace SistemaFinca
         public FormVR_Eliminar()
         {
             InitializeComponent();
+
+
         }
 
-        private void btnRegistrar_Click(object sender, EventArgs e)
+        private void btnEliminar_Click(object sender, EventArgs e)
         {
             if (this.idretiro == "")
             {
                 MessageBox.Show("Seleccione un retiro", "Vuelva a intentar", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
-            using (NpgsqlConnection connection = new NpgsqlConnection(FormLogin.connectionString))
-            {
-                try
-                {
-                    connection.Open();
-                    MessageBox.Show($"{this.idcontrato}, {this.idretiro}", "Vuelva a intentar", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    String commString2 = $"UPDATE contrato SET cantidadretirada = cantidadretirada - (SELECT cantidadleche " +
-                        $"FROM retiro WHERE idretiro = {this.idretiro}) WHERE idcontrato = {this.idcontrato}";
-                    NpgsqlCommand comm2 = new NpgsqlCommand(commString2, connection);
-                    int resultado = comm2.ExecuteNonQuery();
 
-                    String commString = $"DELETE FROM retiro WHERE idretiro = {this.idretiro}";
-                    NpgsqlCommand comm = new NpgsqlCommand(commString, connection);
-                    int resultado2 = comm.ExecuteNonQuery();
-                    if (resultado > 0 && resultado2 > 0)
+            if (MessageBox.Show($"Se eliminará el retiro {this.idretiro} del contrato {this.idcontrato}", "Comprobar", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(FormLogin.connectionString))
+                {
+                    try
                     {
-                        MessageBox.Show("Retiro eliminado exitosamente", "Eliminación exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        this.idretiro = "";
-                        lstRetiros.Items.Clear();
-                        getRetiros();
-                        return;
+                        connection.Open();
+
+                        String commString2 = $"UPDATE contrato SET cantidadretirada = cantidadretirada - (SELECT cantidadleche " +
+                            $"FROM retiro WHERE idretiro = {this.idretiro}) WHERE idcontrato = {this.idcontrato}";
+                        NpgsqlCommand comm2 = new NpgsqlCommand(commString2, connection);
+                        int resultado = comm2.ExecuteNonQuery();
+
+                        String commString = $"DELETE FROM retiro WHERE idretiro = {this.idretiro}";
+                        NpgsqlCommand comm = new NpgsqlCommand(commString, connection);
+                        int resultado2 = comm.ExecuteNonQuery();
+                        if (resultado > 0 && resultado2 > 0)
+                        {
+                            MessageBox.Show("Retiro eliminado exitosamente", "Eliminación exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            this.idretiro = "";
+                            lstRetiros.Rows.Clear();
+                            getRetiros();
+                            return;
+                        }
+                    }
+                    catch (NpgsqlException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        if (connection.State == ConnectionState.Open)
+                        {
+                            connection.Close();
+                        }
                     }
                 }
-                catch (NpgsqlException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                finally
-                {
-                    if (connection.State == ConnectionState.Open)
-                    {
-                        connection.Close();
-                    }
-                }
-            }
+            }  
         }
 
         private void btnBuscar_Click(object sender, EventArgs e)
@@ -80,6 +86,7 @@ namespace SistemaFinca
 
         private void getRetiros()
         {
+            lstRetiros.Rows.Clear();
             using (NpgsqlConnection connection = new NpgsqlConnection(FormLogin.connectionString))
             {
                 try
@@ -113,6 +120,7 @@ namespace SistemaFinca
                     NpgsqlCommand comm2 = new NpgsqlCommand(commString2, connection);
                     using (NpgsqlDataReader reader = comm2.ExecuteReader())
                     {
+
                         if (!reader.HasRows)
                         {
                             MessageBox.Show("No existen retiros", "Vuelve a intentar", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -120,14 +128,15 @@ namespace SistemaFinca
                         }
                         while (reader.Read())
                         {
-                            String cantidad = reader.GetInt32(0).ToString();
-                            String fecha = reader.GetDateTime(1).ToString();
-                            String estado = reader.GetBoolean(2) ? "Pagado" : "No pagado";
-                            ListViewItem item = new ListViewItem(cantidad);
-                            item.SubItems.Add(fecha);
-                            item.SubItems.Add(estado);
-                            item.SubItems.Add(reader.GetInt32(3).ToString());
-                            lstRetiros.Items.Add(item);
+
+                            string cantidad = reader.GetInt32(0).ToString();
+                            string fecha = reader.GetDateTime(1).ToString();
+                            string estado = reader.GetBoolean(2) ? "Pagado" : "No pagado";
+                            string idRetiro = reader.GetInt32(3).ToString();
+
+                            // Agrega una nueva fila al DataGridView con los valores actuales
+                            lstRetiros.Rows.Add(cantidad, fecha, estado, idRetiro);
+                            lstRetiros.ClearSelection();
                         }
                     }
                 }
@@ -145,46 +154,14 @@ namespace SistemaFinca
             }
         }
 
-        public static bool fechaEsValida(string fecha)
+        private void lstRetiros_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            Regex regex = new Regex(@"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{2}$");
-            return regex.IsMatch(fecha);
-        }
-
-        public static bool cantidadEsValida(string cantidad)
-        {
-            Regex regex = new Regex(@"^[1-9]\d{0,3}$");
-            return regex.IsMatch(cantidad);
-        }
-
-        public static bool fechaEsPosterior(string fechaInicioStr, string fechaFinStr)
-        {
-            DateTime fechaInicio, fechaFin;
-
-            if (DateTime.TryParseExact(fechaInicioStr, "dd/MM/yy", null, System.Globalization.DateTimeStyles.None, out fechaInicio) &&
-                DateTime.TryParseExact(fechaFinStr, "dd/MM/yy", null, System.Globalization.DateTimeStyles.None, out fechaFin))
+            if (e.RowIndex > -1)
             {
-                return fechaFin > fechaInicio;
-            }
-            return false;
-        }
+                lstRetiros.ClearSelection();
+                lstRetiros.Rows[e.RowIndex].Selected = true;
 
-        public static string ConvertirFecha(string fechaOriginalStr)
-        {
-            DateTime fechaOriginal;
-
-            if (DateTime.TryParseExact(fechaOriginalStr, "dd/MM/yy", null, System.Globalization.DateTimeStyles.None, out fechaOriginal))
-            {
-                return fechaOriginal.ToString("yyyy-MM-dd");
-            }
-            return fechaOriginalStr;
-        }
-
-        private void lstRetiros_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
-        {
-            if (lstRetiros.SelectedItems.Count > 0)
-            {
-                this.idretiro = lstRetiros.SelectedItems[0].SubItems[3].Text;
+                this.idretiro = lstRetiros.Rows[e.RowIndex].Cells[3].Value.ToString();             
             }
         }
     }
